dfde058c689f89ad7d40167de8c038023b4c6689
h0tcat <zer0busamx@yahoo.co.jp>
2020-05-23 20:13:37 +0900

    First Commit

---
+++ Assets/BallScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallScript : MonoBehaviour
+{
+    public GameObject BallPrefab;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (this.BallPrefab != null)
+            {
+                GameObject ball = GameObject.Instantiate(this.BallPrefab);
+                ball.GetComponent<Rigidbody>().velocity = new Vector3(0, 0, -25);
+            }
+        }
+    }
+}
